8c1b7cb9048c2704616a036e37188079bf10cf2f
Sudheendra Kumar <sudhy.k@gmail.com>
2014-12-17 14:15:48 -0500

    Initial Commit

---
+++ sRestClient/sRestClient.h
@@ -0,0 +1,13 @@
+//
+//  sRestClient.h
+//  sRestClient
+//
+//  Created by Sudheendra K Kaanugovi on 12/17/14.
+//  Copyright (c) 2014 Sudhy Studios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface sRestClient : NSObject
+
+@end
---
+++ sRestClient/sRestClient.m
@@ -0,0 +1,13 @@
+//
+//  sRestClient.m
+//  sRestClient
+//
+//  Created by Sudheendra K Kaanugovi on 12/17/14.
+//  Copyright (c) 2014 Sudhy Studios. All rights reserved.
+//
+
+#import "sRestClient.h"
+
+@implementation sRestClient
+
+@end
